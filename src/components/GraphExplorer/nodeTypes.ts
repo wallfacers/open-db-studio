d509c36971d0fed9e1faeeead852684bde5dd3ad
wallfacers <wallfacerswu@gmail.com>
2026-03-20 17:09:05 +0800

    feat(graph): nodeTypes 注册 link 节点类型

--- src/components/GraphExplorer/nodeTypes.ts
+++ src/components/GraphExplorer/nodeTypes.ts
@@ -2,9 +2,11 @@ import type { NodeTypes } from '@xyflow/react';
 import { TableNodeComponent } from './GraphNodeComponents';
 import { MetricNodeComponent } from './GraphNodeComponents';
 import { AliasNodeComponent } from './GraphNodeComponents';
+import { LinkNodeComponent } from './GraphNodeComponents';
 
 export const nodeTypes: NodeTypes = {
   table: TableNodeComponent,
   metric: MetricNodeComponent,
   alias: AliasNodeComponent,
+  link: LinkNodeComponent,
 };
